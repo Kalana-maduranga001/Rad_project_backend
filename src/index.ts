b6e0d038d0f48241acd32ae2ad78d162e5ff52bf
Kalana Maduranga <kalanamadu2000@gmail.com>
2026-01-01 23:25:43 +0530

    Initial backend commit - 2

--- src/index.ts
+++ src/index.ts
@@ -15,11 +15,11 @@ const app = express()
 app.use(express.json())
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174" ,"https://rad-project-fronted-3wp3.vercel.app/"],
+    origin: ["http://localhost:5173","https://rad-project-fronted-3wp3.vercel.app/"],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"]
   })
 )
-
+//  "http://localhost:5174" 
 app.use("/api/v1/auth", authRouter)
 app.use("/api/v1/product", productRouter)
 app.use("/api/v1/orders", orderRouter)
